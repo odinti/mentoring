5f4a6d4ecb1263423303b62a67c239d1c822d30a
Odynn <admin@kaoz.space>
2020-04-15 16:14:51 -0500

    Add 404 Page

--- src/index.tsx
+++ src/index.tsx
@@ -25,11 +25,15 @@ ReactDOM.render(
                         </li>
                     </Route>
 
-                    <Route path="/">
+                    <Route path="/" exact>
                         <li>
                             <span className="px-4 py-2 block shadow-md rounded  text-gray-500">Home</span>
                         </li>
                     </Route>
+
+                    <li>
+                        <Link to="/" className="px-4 py-2 block shadow-md rounded">Home</Link>
+                    </li>
                 </Switch>
 
 
@@ -40,6 +44,12 @@ ReactDOM.render(
             <Switch>
                 <Route path="/address/:id">
                     <Address/>
+                </Route>
+                <Route path="/404">
+                    <div  className="text-center">
+                        <h1 className="text-6xl">404</h1>
+                        <p className="text-3xl">Not Found</p>
+                    </div>
                 </Route>
                 <Route path="/">
                     <Home/>
